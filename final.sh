1e3cc8f763b2aee39e48f45c3b939c10ffbfe422
israel05 <i.delamo@gmail.com>
2019-10-28 19:54:47 +0100

    archivo suoma de todo llamado final.sh

--- final.sh
+++ final.sh
@@ -8,6 +8,41 @@
 #--title "Información" \
 #--msgbox 'El parseador es un programe en pruebas, no lo use en explotacion.' 10 30
 
+
+let retornoLlamadaAdialogo
+function llamadaAdialogo(){
+OUTPUT="/tmp/input.txt"
+
+# Dibuja el InpuntBox
+dialog --title "El nombre del admin LDAP" \
+--backtitle "Parseador de CSV a LDIP" \
+--inputbox "Indica el nombre del admin LDAP (suele ser admin)" 8 60 2>$OUTPUT
+
+# lee lo que está escribiendo el usuario
+respose=$?
+
+# lee lo que hay en OUTPUT usando la redireccion $OUPUT 
+name=$(<$OUTPUT)
+
+# Entra en el case
+case $respose in
+  0) echo "hola $name" ;;
+  1) echo "Cancel pressed." ;;
+  255) echo "[ESC] key pressed."
+esac
+
+
+#borrar ese archivo temporal que solo guarda mierda
+rm /tmp/input.txt
+
+#hace un retorno de la variable
+retornoLlamadaAdialogo=$name
+}
+
+
+
+
+
 #un menu 
 
 INPUT=/tmp/menu.sh.$$
@@ -34,6 +69,9 @@ case $menuitem in
 	Nombre) echo "Elegiste nombre";;
 	Servidor) echo "Elegiste servidor";;
 	Extension) echo "Elegiste extension";;
-	Origencsv) echo "Elegiste origen"; /bin/bash ./dialogo.sh;;
+	Origencsv) echo "Elegiste origen"; llamadaAdialogo;;
 	Exit) echo "Bye"; break;;
 esac
+
+echo "FINAL"
+echo $retornoLlamadaAdialogo
